476495b00ad71c3702d0da70d32845480807fcb3
lbyanista <mlabrayj@student.1337.ma>
2021-10-08 12:35:50 +0100

    change on cd.c

--- srcs/cd.c
+++ srcs/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ael-mezz <ael-mezz@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: mlabrayj <mlabrayj@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 18:16:59 by mlabrayj          #+#    #+#             */
-/*   Updated: 2021/10/07 11:40:05 by ael-mezz         ###   ########.fr       */
+/*   Updated: 2021/10/08 12:35:09 by mlabrayj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,40 @@
 
 int	cd(t_data *data, char *prototype)
 {
-	struct dirent	*de;// Pointer for directory entry
-    int				ret;
-	char			s[1000];
-	DIR				*dr;
+	// struct dirent	*de;// Pointer for directory entry
+    // int				ret;
+	// char			s[1000];
+	// DIR				*dr;
 
-    // opendir() returns a pointer of DIR type. 
-	dr = opendir(".");
-	// printf("%s current\n", getcwd(s, 1000));
-	ret = chdir(prototype);
+    // // opendir() returns a pointer of DIR type. 
+	// dr = opendir(".");
+	// // printf("%s current\n", getcwd(s, 1000));
+	// ret = chdir(prototype);
 	// printf("%s changed\n", getcwd(s, 1000));
 	// printf("ret = %d\n", ret);
-	if (dr == NULL) // opendir returns NULL if couldn't open directory
-	{
-		printf("Could not open current directory" );
-		return (0);
-	}
-    // for readdir()
-	// while ((de = readdir(dr)) != NULL)
+	// if (ret == -1 )
 	// {
-	// 	printf("%s\n", de->d_name);
-    //     // if(ft_strcmp(de->d_name, av[1]))
-    //     //     readdir(dr);
+	// 	// char *home = getenv("HOME");
+	// 	chdir(getenv("HOME"));
 	// }
-	closedir(dr);
+	// if (dr == NULL) // opendir returns NULL if couldn't open directory
+	// {
+	// 	printf("Could not open current directory" );
+	// 	return (0);
+	// }
+    // // for readdir()
+	// // while ((de = readdir(dr)) != NULL)
+	// // {
+	// // 	printf("%s\n", de->d_name);
+    // //     // if(ft_strcmp(de->d_name, av[1]))
+    // //     //     readdir(dr);
+	// // }
+	// closedir(dr);
+
+	if (prototype)
+		if (chdir(prototype))
+			printf("%s: %s%s No such file or directory\n", "minishell: cd" ,prototype,":");
+	else
+		chdir(getenv("HOME"));
 	return (0);
 }
